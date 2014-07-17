03e79a58bf04a4320c614787bcba1fefb80d08b9
pptalent <wayneup@163.com>
2014-07-17 22:58:12 +0800

    experiment

--- js/main.js
+++ js/main.js
@@ -2,3 +2,17 @@ var app=angular.module('jrex',[]);
 app.run(function($rootScope){
   $rootScope.name="wangzixiao";
 });
+app.directive("myDirective",function(){
+    return{
+        restrict:"A",
+        replace:true,//不会包裹着template了，而是替换
+        template:"<a>{{myFriend}}</a>",
+        scope:{
+            myFriend:'@'//copy data from dom to isolate scope
+        },
+        controller:function($scope){
+            //directive自己的controller，在这里定义的都是独立变量，没有继承关系
+//            $scope.t="wayne0821";
+        }
+    }
+})
