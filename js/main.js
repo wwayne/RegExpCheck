9e244d6317146e5749cc0c5a258e73b3f8e9bed2
pptalent <wayneup@163.com>
2014-07-19 09:25:06 +0800

    improve core math

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 var app=angular.module('jrex',[]);
-app.run(function($rootScope,$timeout){
-  $timeout(function(){
-    $rootScope.href="http://www.sina.com.cn";
-  },2000); //2秒以后执行
-
-});
 app.directive("waynePick",function($compile){
     return{
         restrict:"A",
@@ -17,32 +11,79 @@ app.directive("waynePick",function($compile){
         controller:function($scope){
             $scope.result='';
             $scope.suffix='';
-
+            $scope.text='';
+            $scope.status=true;
             $scope.$watch('expression+text+suffix',function(){
               if($scope.expression.length>0){
+                //输入表达式
+                  if($scope.text.length>0){
+                      //也输入了文本
+                      var expression=new RegExp($scope.expression,$scope.suffix),
+                          text=$scope.text;
+                      var matchArray=text.match(expression);
+                      if(matchArray!=null){
+                          //有匹配结果
+                          if(matchArray[0].length>0){
+                              $scope.status=true;
+                              var i,
+                                  indexArray=[],
+                                  nextResult,
+                                  splitText=text.split(""),
+                                  spanElement=["<span class='match-span'>","</span>"],
+                                  span;
+                              do{
+                                  nextResult=expression.exec(text);
+                                  if(nextResult!=null){
+                                      indexArray.push(nextResult.index);
+                                      indexArray.push(nextResult.index+nextResult[0].length);
+                                  }
+                              }while(nextResult!=null);
+                              for(i=indexArray.length-1;i>=0;i--){
+                                   span=spanElement[i % 2];
+                                   splitText.splice(indexArray[i],0,span);
+                              }
+                              $scope.result=splitText.join("");
+                          }
+                          else{
+                              //匹配的是空字符串
+                              $scope.status=false;
+                              $scope.result=text;
 
-                var expression=new RegExp($scope.expression,$scope.suffix),
-                    text=$scope.text;
-                var boolResult=expression.test(text);
-                if(boolResult){
-                  var textForReplace=text.match(expression)[0];
-                  $scope.result=text.replace(expression,"<span class='match-span'>"+textForReplace+"</span>");
+                          }
+                      }
+                      else{
+                          //无匹配结果
+                          $scope.status=false;
+                          $scope.result=text;
+                      }
+                  }
+                  else{
+                     //输入了表达式，没有输入文本
+                      text=$scope.text;
+                      $scope.status=true;
+                      $scope.result=text;
+                  }
 
-                }
-                else{
-                  $scope.result=text;
-                  console.log("nothing");
-                }
               }
               else{
-
+                //表达式没有输入时
+                  var text=$scope.text;
+                  $scope.status=true;
+                  $scope.result="";
               }
             });
         },
         link:function($scope, element, attrs) {
           // scope.rootDirectory = 'images/';
           $scope.$watch('expression+text+suffix',function(){
-            var sentence="<p>"+$scope.result+"</p>";
+            var status=$scope.status,
+                sentence;
+            if(status){
+                  sentence="<div class='match-result match-place' ><p>"+$scope.result+"</p></div>";
+            }
+            else{
+                  sentence="<div class='match-result-false match-place' ><p>No match result</p></div>";
+            }
             element.html(sentence) ;
             $compile( element.contents() )($scope);
           });
@@ -50,4 +91,18 @@ app.directive("waynePick",function($compile){
         }
 
     }
-})
+});
+function combineArray(originArray){
+    var newArray=[], i,element,originLength;
+    do{
+        originLength=originArray.length;
+        element=originArray[originLength-1];
+        newArray.push(element);
+        for(i=originLength-1;i>=0;i--){
+            if(originArray[i]===element){
+                originArray.splice(i,1);
+            }
+        }
+    }while(originArray.length>0)
+    return newArray;
+}
